e485ae23aae0a1ef0f067bafeb53626aaf05116a
FindMalek <malekgarahellalbus@gmail.com>
2023-07-07 19:58:11 +0100

    (1.1.3) - Expected Server Component | Navbar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang='en' className={ cn('bg-white text-slate-900 antialiased light', inter.className) }>
 
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+          {/* @ts-expect-error Server Component */}
           <Navbar />
 
           <div className='container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
